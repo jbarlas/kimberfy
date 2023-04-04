8c83e93e1b546c96f03450c67707484ec218c448
jbarlas <justin_barlas@brown.edu>
2023-04-04 00:29:51 -0400

    firebase env sux

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,26 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { getDatabase, ref, update } from "firebase/database";
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_firebase_apiKey,
-  authDomain: "kimberfy.firebaseapp.com",
-  databaseURL: process.env.REACT_APP_firebase_databaseURL,
-  projectId: "kimberfy",
-  storageBucket: "kimberfy.appspot.com",
-  messagingSenderId: process.env.REACT_APP_firebase_messagingSenderId,
-  appId: process.env.REACT_APP_firebase_appId,
-};
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_firebase_apiKey,
+//   authDomain: "kimberfy.firebaseapp.com",
+//   databaseURL: process.env.REACT_APP_firebase_databaseURL,
+//   projectId: "kimberfy",
+//   storageBucket: "kimberfy.appspot.com",
+//   messagingSenderId: process.env.REACT_APP_firebase_messagingSenderId,
+//   appId: process.env.REACT_APP_firebase_appId,
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 
-const auth = getAuth(app);
-const db = getDatabase(app);
+const auth = getAuth();
+const db = getDatabase();
 
 export const firebaseSignIn = async (email) => {
   return signInWithEmailAndPassword(auth, email, "testing123")

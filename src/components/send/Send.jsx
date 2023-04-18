35edf34328c182d9c81829a7e56a3b3115e03823
jbarlas <justin_barlas@brown.edu>
2023-04-18 15:10:51 -0400

    hard to debug padding

--- src/components/send/Send.jsx
+++ src/components/send/Send.jsx
@@ -73,7 +73,6 @@ export default function Send() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        paddingBottom: "100px",
       }}
     >
       {songPlaying && device ? (
